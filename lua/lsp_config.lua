b23919c1b8ce96be98dac13cefd75830e473aa1a
33r01b <33r01b@gmail.com>
2022-12-15 01:26:33 +0300

    remap

--- lua/lsp_config.lua
+++ lua/lsp_config.lua
@@ -29,14 +29,14 @@ local on_attach = function(client, bufnr)
   --vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
   vim.keymap.set('n', 'q', vim.lsp.buf.hover, bufopts)
   vim.keymap.set('n', 'Q', vim.lsp.buf.signature_help, bufopts)
+  vim.keymap.set('n', '<leader>e', function() vim.lsp.buf.format { async = true } end, bufopts)
+  vim.keymap.set('n', '<leader>r', vim.lsp.buf.rename, bufopts)
   vim.keymap.set('n', '<leader>wa', vim.lsp.buf.add_workspace_folder, bufopts)
   vim.keymap.set('n', '<leader>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
   vim.keymap.set('n', '<leader>wl', function()
     print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
   end, bufopts)
-  vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, bufopts)
   vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, bufopts)
-  vim.keymap.set('n', '<leader>f', function() vim.lsp.buf.format { async = true } end, bufopts)
 end
 
 -- completion
